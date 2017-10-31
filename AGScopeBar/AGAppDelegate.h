e9ff9ef05b35c031afce837c8f7f4fe4d16524e3
Seth Willits <seth@freaksw.com>
2017-10-31 09:28:21 -0700

    Getting rid of MGScopeBar comparison

--- AGScopeBar/AGAppDelegate.h
+++ AGScopeBar/AGAppDelegate.h
@@ -7,19 +7,15 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import "MGScopeBar.h"
 @class AGScopeBar;
 
 
 @interface AGAppDelegate : NSObject <NSApplicationDelegate, MGScopeBarDelegate>
 {
 	IBOutlet AGScopeBar * agScopeBar;
-	IBOutlet MGScopeBar * mgScopeBar;
 	IBOutlet NSWindow * window;
 	IBOutlet NSMenu * agMenu;
-	IBOutlet NSMenu * mgMenu;
 	IBOutlet NSView * agAccessoryView;
-	IBOutlet NSView * mgAccessoryView;
 }
 
 @end
--- AGScopeBar/AGAppDelegate.m
+++ AGScopeBar/AGAppDelegate.m
@@ -8,7 +8,6 @@
 
 #import "AGAppDelegate.h"
 #import "AGScopeBar.h"
-#import "MGScopeBar.h"
 
 
 @implementation AGAppDelegate
@@ -42,116 +41,8 @@
 	[group addItemWithIdentifier:@"warn" title:@"Warnings"];
 	group.selectionMode = AGScopeBarGroupSelectAny; //tLeastOne;
 	group.showsSeparator = NO;
-	
-	
-	[mgScopeBar reloadData];
 }
 
-
-
-
-
-
-
-
-- (NSUInteger)numberOfGroupsInScopeBar:(MGScopeBar *)theScopeBar;
-{
-	return 3;
-}
-
-- (NSArray *)scopeBar:(MGScopeBar *)theScopeBar itemIdentifiersForGroup:(NSInteger)groupNumber;
-{
-	if (groupNumber == 0) return [NSArray arrayWithObjects:@"a", @"r", @"m", nil];
-	if (groupNumber == 1) return [NSArray arrayWithObjects:@"all", @"dict", @"thes", nil];
-	if (groupNumber == 2) return [NSArray arrayWithObjects:@"errs", @"warn", nil];
-	return nil;
-}
-
-- (NSString *)scopeBar:(MGScopeBar *)theScopeBar labelForGroup:(NSInteger)groupNumber;
-{
-	if (groupNumber == 0) return nil;
-	if (groupNumber == 1) return @"Radio:";
-	if (groupNumber == 2) return @"Multi:";
-	return nil;
-}
-
-- (MGScopeBarGroupSelectionMode)scopeBar:(MGScopeBar *)theScopeBar selectionModeForGroup:(NSInteger)groupNumber;
-{
-	if (groupNumber == 0) return MGMultipleSelectionMode;
-	if (groupNumber == 1) return MGRadioSelectionMode;
-	if (groupNumber == 2) return MGMultipleSelectionMode;
-	return 0;
-}
-
-
-
-- (NSString *)scopeBar:(MGScopeBar *)theScopeBar titleOfItem:(NSString *)identifier inGroup:(NSInteger)groupNumber;
-{
-	if (groupNumber == 0) {
-		if ([identifier isEqual:@"m"]) return @"Menu";
-		return nil;
-	}
-	
-	if (groupNumber == 1) {
-		if ([identifier isEqual:@"all"]) return @"All";
-		if ([identifier isEqual:@"dict"]) return @"Dictionary";
-		if ([identifier isEqual:@"thes"]) return @"Thesaurus";
-	}
-	
-	if (groupNumber == 2) {
-		if ([identifier isEqual:@"errs"]) return @"Errors";
-		if ([identifier isEqual:@"warn"]) return @"Warnings";
-	}
-	
-	return nil;
-}
-
-
-
-- (NSImage *)scopeBar:(MGScopeBar *)theScopeBar imageForItem:(NSString *)identifier inGroup:(NSInteger)groupNumber;
-{
-	if (groupNumber == 0) {
-		if ([identifier isEqual:@"a"]) return [NSImage imageNamed:@"NSAddTemplate"];
-		if ([identifier isEqual:@"r"]) return [NSImage imageNamed:@"NSRemoveTemplate"];
-	}
-	
-	return nil;
-}
-
-
-
-- (NSMenu *)scopeBar:(MGScopeBar *)theScopeBar menuForItem:(NSString *)identifier inGroup:(NSInteger)groupNumber;
-{
-	if (groupNumber == 0) {
-		if ([identifier isEqual:@"m"]) return mgMenu;
-	}
-	
-	return nil;
-}
-
-
-
-- (BOOL)scopeBar:(MGScopeBar *)theScopeBar showSeparatorBeforeGroup:(NSInteger)groupNumber;
-{
-	if ((groupNumber == 0) || (groupNumber == 2)) {
-		return NO;
-	}
-	
-	return YES;
-}
-
-
-- (NSView *)accessoryViewForScopeBar:(MGScopeBar *)theScopeBar;
-{
-	return mgAccessoryView;
-}
-
-
-
-- (void)scopeBar:(MGScopeBar *)theScopeBar selectedStateChanged:(BOOL)selected forItem:(NSString *)identifier inGroup:(NSInteger)groupNumber;
-{
-	NSLog(@"MG item selection changed");
-}
 
 
 - (void)scopeBar:(AGScopeBar *)theScopeBar item:(AGScopeBarItem *)item wasSelected:(BOOL)selected;
